69e4493fd2e989e69f3f984d091e89f5e0c3295c
Nayana Loivos <loivosdev@gmail.com>
2022-04-15 11:01:01 -0300

    Correção do código que leva o json ao HTML

--- fetch.js
+++ fetch.js
@@ -1,15 +1,19 @@
 (async() => {
     //abrir o arquivo de dados (json)
-    const respose = await fetch('./criptomoedas.json');
-    const data = await respose.json();
+    const response = await fetch('./criptomoedas.json');
+    const data = await response.json();
+    const container = document.querySelector('.container')
 
     //formatar os dados para enviar para o html
-     let htmlList;
-     htmlList = data.map((abv) => {
-         return {abreviacaoCriptomoeda: abv, preco: 56};
-     });
-
 
      //colocar no html
-     document.querySelector('.container').innerHTML = htmlList;
+    for (let i = 0; i < data.length; i++) {
+        let estruturaDiv = document.createElement('div');
+        estruturaDiv.innerHTML =
+            `<div class="">
+                <h2>${data[i].criptomoeda}</h2>
+                <h3>${data[i].preco}</h3>
+            </div>`;
+        container.append(estruturaDiv);
+    }
 }  )();
